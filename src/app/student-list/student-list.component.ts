bb6c6d970be84852fbd08e19cb94369d15aa4cd6
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2024-06-04 10:29:00 -0400

    Add dialog box

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Students } from '../students';
 import { NgFor, NgIf } from '@angular/common'; 
 import { HttpClient } from '@angular/common/http';
@@ -10,12 +10,26 @@ import { MatButton } from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-student-list',
   standalone: true,
   imports: [ FormsModule, NgIf, NgFor, StudentDetailComponent, MatGridListModule, MatButton, MatDividerModule, MatListModule ,
-    MatCardModule
+    MatCardModule, MatButtonModule, MatIconModule
   ],
   templateUrl: './student-list.component.html',
   styleUrl: './student-list.component.css'
@@ -28,8 +42,16 @@ export class StudentListComponent {
   baseUrl : string = 'https://json-server-vercel-matt-krauskopfs-projects.vercel.app/';
   studentsUrl : string = this.baseUrl + "students";
 
-  constructor(private http : HttpClient) {
+  constructor(private http : HttpClient, public dialog : MatDialog) {
     this.getStudents().subscribe(s => this.students = s);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddStudentDialog, {data : {firstName: "", lastName: ""}});
+
+    dialogRef.afterClosed().subscribe(newStudent => {
+      this.addStudent(newStudent.firstName, newStudent.lastName);
+    });
   }
 
   onSelect(student : Students) : void {
@@ -43,12 +65,13 @@ export class StudentListComponent {
       catchError(this.handleError<Students[]>('getStudents')));
   }
 
-  addStudent(name : String) {
-    const trimmedName : string = name.trim();
-    if (trimmedName) {
+  addStudent(firstName : String, lastName : String) {
+    const trimmedFirstName : string = firstName.trim();
+    const trimmedLastName : string = lastName.trim();
+    if (trimmedFirstName && trimmedLastName) {
       let newStudent : Students = {
-        "firstName" : trimmedName,
-        "lastName": "",
+        "firstName" : trimmedFirstName,
+        "lastName": trimmedLastName,
         "id": this.getNewStudentId(),
         "isActive": true,
         "enrolledCourses": []
@@ -81,3 +104,33 @@ export class StudentListComponent {
   }
 }
 
+export interface NewStudent {
+  firstName: string;
+  lastName: string;
+}
+
+@Component({
+  selector: "dialog-add-student",
+  standalone: true,
+  templateUrl: "./add-student-dialog.html",
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ]
+})
+export class AddStudentDialog {
+  constructor(
+    public dialogRef: MatDialogRef<AddStudentDialog>,
+    @Inject(MAT_DIALOG_DATA) public newStudent: NewStudent,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
