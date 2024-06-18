202602845e5a145d63021e6ea32581c655ccc2b1
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2024-06-18 12:53:49 -0400

    Restyle courses tab

--- src/app/course/course-detail/course-detail.component.ts
+++ src/app/course/course-detail/course-detail.component.ts
@@ -4,14 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { Courses } from '../courses';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-course-detail',
   standalone: true,
-  imports: [NgIf, FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [NgIf, FormsModule, MatFormFieldModule, MatInputModule, MatCardModule],
   templateUrl: './course-detail.component.html',
   styleUrl: './course-detail.component.scss'
 })
 export class CourseDetailComponent {
-  @Input() course? : Courses;
+  @Input() course! : Courses;
 }
